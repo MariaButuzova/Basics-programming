9f29c3609b5c1b67fba96945febb03b74d349c0c
MariaButuzova <maria.butuzova@gmail.com>
2015-12-13 20:41:42 +0200

    HomeTask1 has added

---
+++ HomeTask1/src/hometask1/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hometask1;
+
+/**
+ *
+ * @author solenko
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String strH = "Hello, ";
+        String strU = "U-Rise";
+        char cFact = '!';
+        boolean bool = true;
+        int iPercent = 100;
+        //double dValue = 0.99;
+        float fValue = 0.99f;
+        //String strA = "There is ";
+        String strB = ", that there are ";
+        String strC = " percent sucess with ";
+        String strD = " happiness.";
+        
+        System.out.println(strH+strU+cFact);
+        System.out.println("There is " + bool + strB + iPercent + strC + fValue 
+        + strD);
+    }
+
+    
+}
