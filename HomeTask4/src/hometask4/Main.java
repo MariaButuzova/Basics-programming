6caab2dce64c0304e9490f2cefef0ae912968c2f
MariaButuzova <maria.butuzova@gmail.com>
2015-12-27 04:55:20 +0200

    HomeTask4 part 3 was done

--- HomeTask4/src/hometask4/Main.java
+++ HomeTask4/src/hometask4/Main.java
@@ -1,6 +1,7 @@
 package hometask4;
 
-import java.math.BigInteger;
+import java.util.*;
+import java.math.*;
 
 /**
  *
@@ -12,14 +13,28 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Test 4 part 2:");
+        Random random = new Random();
+        int[] arrayInt = new int[random.nextInt(1000) + 1];
+        for (int i = 0; i < arrayInt.length; ++i){
+            arrayInt[i] = random.nextInt(2001) - 1000;
+            System.out.println(arrayInt[i]);
+        }
+        System.out.println("Test 4.1:");
+        System.out.println(getSumOfArrayNumbers(arrayInt));
+        System.out.println("Test 4.2:");
         System.out.println(getPowerFunction(45, 5));
-        System.out.println("Test 4 part 3.1:");
+        System.out.println("Test 4.3.1 getFibonacciNumbers:");
         getFibonacciNumbers(6);
-        System.out.println("Test 4 part 3.2:");
+        System.out.println("Test 4.3.2 getFibonacciNumbersRecursion:");
         for (int i = 0; i < 6; ++i){
             System.out.println(getFibonacciNumbersRecursion(i + 1));
         }
+    }
+    
+    public static int getSumOfArrayNumbers(int[] number) {
+        int sum = 0;
+        for (int i : number) sum += i;
+        return sum;
     }
     
     public static int getPowerFunction(int n, int m) {
