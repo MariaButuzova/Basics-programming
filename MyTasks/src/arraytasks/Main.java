c8fe8227d6c6e3c1a0587a7ee7fc8e79d2f6e67a
MariaButuzova <maria.butuzova@gmail.com>
2016-01-16 05:53:32 +0200

    inputdata was modified

--- MyTasks/src/arraytasks/Main.java
+++ MyTasks/src/arraytasks/Main.java
@@ -13,7 +13,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] intArray = InputDataUtils.getIntArrayRandom(-1, 1, 0);
+        int[] intArray = InputDataUtils.getIntArrayRandom(-1, 1, 10);
         double[] doubleArray = InputDataUtils.getDoubleArrayRandom(-1, 1, 30);
         System.out.println(Arrays.toString(intArray));
         System.out.println(ArrayUtils.getMaxIndexes(intArray));
